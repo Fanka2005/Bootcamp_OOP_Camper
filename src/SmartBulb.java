29cafb4560c2d5f2625a9e9543087f341d7b25fe
fsun0022 <fsun0022@student.monash.edu>
2025-08-03 23:11:26 +1000

    (Week 1) Add SmartBulb class/file along with its attributes, constructors, getters, and setters - this class represent a smart bulb - add 3 attributes which are status, colour, and consumptionToday - add getters and setters for each attribute - for attribute status, create two setters which are turnOn() to turn on the bulb (set the status to true) and turnOff() to turn off the bulb (set the status to false) - Add Constructor - Add Constructor Overloading to initialise the object (bulb) state.

---
+++ src/SmartBulb.java
@@ -0,0 +1,48 @@
+public class SmartBulb {
+    //Private Attributes
+        private boolean status;
+        private String colour;
+        private double consumptionToday; //in Watts
+
+    //Constructors
+    public SmartBulb(){
+
+    }
+
+    //Constructors Overloading
+    public SmartBulb(boolean _status, String _colour){
+        this.status = _status;
+        this.colour = _colour;
+    }
+
+
+    //Getters and Setters
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void turnOn() {
+        this.status = true;
+    }
+
+    public void turnOff() {
+        this.status = false;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public double getConsumptionToday() {
+        return consumptionToday;
+    }
+
+    public void setConsumptionToday(double consumptionToday) {
+        this.consumptionToday = consumptionToday;
+    }
+
+}
