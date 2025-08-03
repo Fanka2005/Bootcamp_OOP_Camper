11834603071350f0c64541e3dcb15d74fe3d1eec
fsun0022 <fsun0022@student.monash.edu>
2025-08-03 20:33:25 +1000

    Print out both the sum and the multiplied value - Modified the Application class to illustrate the multiplyTwoNumbers() method for performing multiplication calculations. This ensures that multiplication functionality is seamlessly integrated into the application flow.
    This commit test the Calculator multiplication functionality to ensure a smooth experience.

--- src/Application.java
+++ src/Application.java
@@ -9,13 +9,15 @@ public class Application {
 
         // a container for the sum
         int result;
+        int result2;
 
         // create a new instance of this class to be able to access the two methods
         Calculator calculator = new Calculator();
 
         if (calculator.isPositive(number1) && calculator.isPositive(number2)) {
             result = calculator.addTwoPositiveNumbers(number1, number2);
-            System.out.println("Both numbers are positive and the sum is " + result);
+            result2 = calculator.multipyTwoNumbers(number1, number2);
+            System.out.println("Both numbers are positive and the sum is " + result + " and the multiplied value is " + result2);
         } else
             System.out.println("Number1 or/and Number2 is/are not positive");
     }
