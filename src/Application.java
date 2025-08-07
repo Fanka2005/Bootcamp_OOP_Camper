6427f603ec09974faa4b39e23723c0889b0be199
fsun0022 <fsun0022@student.monash.edu>
2025-08-07 11:39:17 +1000

    (Week 2) Create a new branch named bootcamp-week-2 and clean this branch - create bootcamp-week-2 branch to work on week 2 project - delete all files that is unrelated to bootcamp project

--- src/Application.java
+++ src/Application.java
@@ -1,24 +1,5 @@
 public class Application {
     public static void main(String[] args) {
 
-        //first integer
-        int number1 = 7;
-
-        //second integer
-        int number2 = 4;
-
-        // a container for the sum
-        int result;
-        int result2;
-
-        // create a new instance of this class to be able to access the two methods
-        Calculator calculator = new Calculator();
-
-        if (calculator.isPositive(number1) && calculator.isPositive(number2)) {
-            result = calculator.addTwoPositiveNumbers(number1, number2);
-            result2 = calculator.multipyTwoNumbers(number1, number2);
-            System.out.println("Both numbers are positive and the sum is " + result + " and the multiplied value is " + result2);
-        } else
-            System.out.println("Number1 or/and Number2 is/are not positive");
     }
 }
