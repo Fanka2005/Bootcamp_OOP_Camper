fab0984e2c4c6f5556a83babd7a75499ec7e7fe7
fsun0022 <fsun0022@student.monash.edu>
2025-08-10 02:24:48 +1000

    (Week 2) Add initial Campsite class and modify Application class  - Implemented the Campsite class to manage camper-related functionality.  - Modified the Application class as the main entry point for the system.  - Established the basic structure and initial method calls for loading and displaying item details.

---
+++ src/Campsite.java
@@ -0,0 +1,10 @@
+public class Campsite {
+    public void simulate() {
+        System.out.println("Welcome to FIT2099 Camping Site");
+
+        // The following method will be implemented in the next few requirements
+        // camper.checkAllItems();
+
+        System.out.println("Glad that you enjoy your time here!");
+    }
+}
