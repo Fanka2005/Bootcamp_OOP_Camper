c2eb6dd63a888389963fff14a34a3bcd8635a67c
fsun0022 <fsun0022@student.monash.edu>
2025-08-12 15:39:19 +1000

    (Week 2) Modify backpack class
    - Remove the 3 item types attribute as it violates the uml design
    - modify the items attribute to add the 3 item types and object inside the array, to align with the uml design

--- src/Backpack.java
+++ src/Backpack.java
@@ -1,8 +1,5 @@
 public class Backpack {
-    private Bottle bottle = new Bottle("Bottle", 1.00, 1.0);
-    private FlintAndSteel flintAndSteel = new FlintAndSteel("Flint and Steel", 0.50);
-    private Bedroll bedroll = new Bedroll("Bedroll", 7.00);
-    private Item[] items = {this.bedroll, this.bottle, this.flintAndSteel};
+    private Item[] items = {new Bottle("Bottle", 1.00, 1.0), new FlintAndSteel("Flint and Steel", 0.50), new Bedroll("Bedroll", 7.00)};
 
     // Getter Method
     public Item[] getItems(){
