650d0a1a5bae9a05616c71f7418c240dd34d3116
fsun0022 <fsun0022@student.monash.edu>
2025-08-12 14:53:16 +1000

    (Week 2) Add FlintAndSteel and Bedroll class as a child class of the Item class
    - Add Constructor in FlintAndSteel and Bedroll class

---
+++ src/Bedroll.java
@@ -0,0 +1,6 @@
+public class Bedroll extends Item{
+    // Constructor
+    public Bedroll(String _name, float _weight){
+        super(_name, _weight);
+    }
+}
---
+++ src/FlintAndSteel.java
@@ -0,0 +1,7 @@
+public class FlintAndSteel extends Item{
+
+    // Constructor
+    public FlintAndSteel(String _name, float _weight){
+        super(_name, _weight);
+    }
+}
