1f51b0c44910778e1a59135c18f5150abb964f67
fsun0022 <fsun0022@student.monash.edu>
2025-08-20 22:08:48 +1000

    (Week 3) Modify Application class
    - iterate  simulation 6 times

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,11 @@
 public class Application {
     public static void main(String[] args) {
         Campsite campsite = new Campsite();
-        campsite.simulate();
+
+        int i = 0;
+        do {
+            campsite.simulate();
+            i += 1;
+        } while (i<6);
     }
 }
