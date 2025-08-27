1aa0738277f974cd99d26564e25449b6acfaff7a
fsun0022 <fsun0022@student.monash.edu>
2025-08-27 15:13:31 +1000

    (Week 4) Add Javadoc to Application classes
    - Add Javadoc which contains a description for a certain code to make it more readable.

--- src/Application.java
+++ src/Application.java
@@ -1,13 +1,20 @@
 import campobjects.Campsite;
 
+/**
+ * <h1>Application class</h1>
+ * The class Application is used to run the campsite simulation.
+ *
+ * @author Fauzanda Lathifanka Sunarko
+ */
 public class Application {
-    public static void main(String[] args) {
-        Campsite campsite = new Campsite();
 
-        int i = 0;
-        do {
-            campsite.simulate();
-            i += 1;
-        } while (i<6);
-    }
+  public static void main(String[] args) {
+    Campsite campsite = new Campsite();
+
+    int i = 0;
+    do {
+      campsite.simulate();
+      i += 1;
+    } while (i < 6);
+  }
 }
